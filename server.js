1f904495393c8418b1e7d208a6b596bbe72c6735
Alberto Gil <agildav@gmail.com>
2018-06-09 23:18:18 -0400

    fixed db connect

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "gil",
-    password: "clave",
-    database: "smartbrain"
+    connectionString: process.env.DATABASE_URL,
+    ssl: "true"
   }
 });
 
