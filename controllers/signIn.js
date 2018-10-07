4d0f88ea05fa437bf3951a6838a81ad7bc20f6ca
Alberto Gil <agildav@gmail.com>
2018-10-07 13:07:55 -0400

    Added redis (dockerized)

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -1,4 +1,7 @@
 const jwt = require("jsonwebtoken");
+const redis = require("redis");
+
+const redisClient = redis.createClient(process.env.REDIS_URL);
 
 const handleSignIn = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
